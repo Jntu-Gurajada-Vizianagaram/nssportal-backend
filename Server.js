34718602b9ee210731b894c0d8aab9d1a7a09900
dsak789 <dsak.official@gmail.com>
2024-04-26 19:18:18 +0530

    NSS Backend

---
+++ Server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require('dotenv').config()
+//Routes Import
+const schemas = require("../Backend_Console/Schemes/AllSchemas");
+const admins = require("../Backend_Console/routes/admin_routes/AdminRoute");
+const updates = require("../Backend_Console/routes/updates_routes/upates_api_routes");
+const downloads=require("../Backend_Console/routes/download_routes/download_routes");
+const latestnews=require("../Backend_Console/routes/latestnews_routes/latestnews_routes");
+//middle ware import
+app.use(express.json());
+
+const session = require("express-session");
+const bodyparser = require("body-parser");
+const cookieparser = require("cookie-parser");
+const con = require("../Backend_Console/apis/config");
+app.use(express.json());
+app.use(cors());
+
+
+app.use(cookieparser());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    key: "userId",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  }),
+);
+
+app.use("/media", express.static("./storage/nss_notifications"));
+app.use("/downloads", express.static("./storage/nss_downloads"));
+app.use("/latestnews", express.static("./storage/nss_latestnews"));
+
+//apis start
+app.use("/api/admins", admins);
+app.use("/api/updates", updates);
+app.use("/api/downloads",downloads);
+app.use("/api/latestnews",latestnews);
+
+// server listener
+const port = 8888;
+app.listen(port, () => {
+  schemas.allSchemas();
+  console.log(`Server ruinning at port no:${port}`);
+});
---
+++ apis/updates_api/UpdatesApi.js
@@ -0,0 +1,104 @@
+const multer = require('multer');
+const fs = require('fs')
+const connection = require('../config')
+require('dotenv').config()
+// const api_ip = process.env.domainip
+const api_ip = 'http://localhost:8888'
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb )=>{
+    return cb(null,'./storage/nss_notifications/')
+  },
+  filename: (req, file, cb)=>{
+    return cb(null,`${file.originalname}`)
+  }
+})
+
+exports.Upload = multer({storage}).single('file')
+
+exports.insert_event =  (req, res) => {
+
+  const  update  = req.body;
+  const  file  = req.file;
+  console.log(update)
+  console.log("File"+file.originalname)
+  const sql = 'INSERT INTO nss_notification_updates ( date, title,  file_path) VALUES (?, ?, ?)';
+  const values = [update.date, update.title,  file.originalname];
+  console.log({values})
+  connection.query(sql, values, (err, result) => {
+    if (err) {
+      console.error('Error inserting data:', err);
+      res.status(500).json({ error: 'Error inserting data' });
+      return;
+    }
+    console.log('Data inserted successfully');
+    res.json({ message: 'Data inserted successfully' });
+  });
+};
+
+exports.delete_event = (req, res) => {
+  const id = req.body.id;
+  const del = `DELETE FROM nss_notification_updates WHERE id = ${id}`;
+  
+  connection.query(del, (err, result) => {
+    if (err) {
+      console.error('Error deleting data:', err);
+      res.status(500).json({ error: 'Error deleting data' });
+      return;
+    }
+    
+    if (result.affectedRows === 0) {
+      console.error('No Records Found');
+      res.status(404).json({ error: 'No Records Found' });
+      return;
+    }
+
+    if (result && result.length > 0 && result[0].file_path) {
+      const filepath = `./storage/nss_notifications/${result[0].file_path}`;
+
+      fs.unlink(filepath, (unlinkErr) => {
+        if (unlinkErr) {
+          console.error('Error removing file:', unlinkErr);
+          res.status(500).json({ error: 'Error removing file' });
+          return;
+        }
+        console.log('File removed successfully');
+      });
+    }
+
+    console.log('Data deleted successfully');
+    res.json({ message: 'Data deleted successfully'});
+  });
+};
+
+
+
+
+
+exports.every_events=(req, res) => {
+  const sql = "SELECT * FROM nss_notification_updates ORDER BY id DESC";
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error retrieving data:', err);
+      res.status(500).json({ error: `Error retrieving data${err} `});
+      return;
+    }
+    const final_events = results.map(eve=>{
+      const filelink =`${api_ip}/media/${eve.file_path}`
+      const outdate=new Date(eve.date)
+
+      return {
+        ...eve,
+        file_link:filelink,
+        day:outdate.getDate(),
+        month: outdate.toLocaleString('en-US', { month: 'short' }),
+        year: outdate.getFullYear(),
+      }
+    })
+
+    console.log('Data retrieved successfully');
+
+    res.json(final_events);
+  });
+};
